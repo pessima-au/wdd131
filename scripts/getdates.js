4145bfd40f93e3454fad474c9b91c7bc3b5082e8
Pessima_au <albertpessima@outlook.com>
2024-04-27 11:28:27 +0000

    updated ss

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const today = new Date().getFullYear();
+const currentyear = document.getElementById("#currentyear")
+currentyear.textContent = currentyear;
+
+const lastModified = document.lastModifiedDate;
+
+const lastModified = document.getElementById("#lastModifiedDatd")
+
+lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
