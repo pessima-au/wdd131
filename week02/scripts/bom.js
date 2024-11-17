62e6930e5603c56ff07774f49246202093967404
pessima_au <albertpessima@outlook.com>
2024-11-17 05:02:44 +0000

    Completed bom app.

---
+++ week02/scripts/bom.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌'
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        deleteButton.addEventListener('click',function() {
+            li.remove();
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    } else {
+        input.focus();
+    }
+  });
+
+
+
