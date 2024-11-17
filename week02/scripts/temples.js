78bfb046b26598602cd69a071d9a5b989b0ecc7a
pessima_au <albertpessima@outlook.com>
2024-11-17 06:49:48 +0000

    completed  temple album

---
+++ week02/scripts/temples.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+
+  
+    const hamburger = document.getElementById("hamburger-menu");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", function() {
+        navLinks.classList.toggle("show"); 
+
+        if (navLinks.classList.contains("show")) {
+            hamburger.innerHTML = "&times;"; 
+        } else {
+            hamburger.innerHTML = "&#9776;"; 
+        }
+    });
+});
