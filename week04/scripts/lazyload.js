4155c9bad35d3e073f4b81b12bfc2021eab50d39
pessima_au <albertpessima@outlook.com>
2025-10-26 21:51:17 +0000

    test

--- week04/scripts/lazyload.js
+++
@@ -1,8 +0,0 @@
-const currentYear = document.getElementById('currentYear');
-const dateModified = document.getElementById("lastModified");
-
-const date = new Date();
-
-currentYear.textContent = date.getFullYear();
-dateModified.textContent = `Last modification: ${date.toLocaleString()}`
-
