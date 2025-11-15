1e85fd35c4c5d746d9010f6b687de512475b6a72
pessima_au <albertpessima@outlook.com>
2025-11-15 22:06:34 +0000

    completed country page.

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,24 @@ document.getElementById("currentYear").textContent = date.getFullYear();
 document.getElementById(
   "lastModified"
 ).innerText = `Last Modified: ${document.lastModified}`;
+
+
+//wind chill calculator
+function calculateWindChill(temperature, windSpeed) {
+  temperature = 27;
+  windSpeed = 8;
+
+  if (temperature <= 10 && windSpeed > 4.8) {
+    return (
+      13.12 +
+      0.6215 * temperature -
+      11.37 * Math.pow(windSpeed, 0.16) +
+      0.3965 * temperature * Math.pow(windSpeed, 0.16)
+    );
+  } else {
+    return "N/A";
+  }
+}
+
+const windchill = (document.querySelector(".wind-chill").innerText =
+  calculateWindChill());
